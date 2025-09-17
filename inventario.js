8b95d18c55135903db315afb579cfa710f10baf9
tan25gonz-ux <tan25gonz@gmail.com>
2025-09-17 06:18:25 +0200

    Update inventario.js3

--- inventario.js
+++ inventario.js
@@ -1,5 +1,5 @@
 import { auth, db } from "./firebase.js";
-import { doc, getDoc, setDoc } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-firestore.js";
+import { doc, getDoc } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-firestore.js";
 
 const materiales = [
   "Hierro", "Aluminio", "Cobre", "Bronce",
@@ -10,11 +10,10 @@ const materiales = [
 const tabla = document.querySelector("#tablaInventario tbody");
 
 async function cargarInventario() {
-  const uid = auth?.currentUser?.uid || "global"; // puede ser por usuario o global
+  const uid = auth?.currentUser?.uid || "anonimo";
   const docRef = doc(db, "inventarios", uid);
   const snap = await getDoc(docRef);
 
-  // inicializar inventario en 0
   const datos = {};
   materiales.forEach(m => datos[m] = 0);
 
@@ -25,7 +24,6 @@ async function cargarInventario() {
     });
   }
 
-  // pintar tabla
   tabla.innerHTML = "";
   materiales.forEach(mat => {
     const fila = `<tr><td>${mat}</td><td>${datos[mat]}</td></tr>`;
